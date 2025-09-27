be5ef4e09991555ae8a0a2d6fe39914bb029b167
Madhur Gupta <madhurgupta107@gmail.com>
2025-09-28 01:27:14 +0530

    fix: correct import statement formatting in frontend config

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { baseSepolia, polygonAmoy,sepolia } from '@reown/appkit/networks'
+import { baseSepolia, polygonAmoy, sepolia } from "@reown/appkit/networks";
 
 // Get projectId from https://dashboard.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
